528761bdfacac72fc2e72a64462d7029c74ea728
Florin David <florin_dav@yahoo.com>
2021-11-16 06:42:46 +0200

    Change phpinfo path

--- 002_scripts/002_centos-lamp.sh
+++ 002_scripts/002_centos-lamp.sh
@@ -34,7 +34,7 @@ mysql -u root -e "SHOW DATABASES";
 cd /vagrant
 yum install -y wget
 sudo -u vagrant wget -q https://raw.githubusercontent.com/flodruid/vagrant/main/001_files/index.html
-sudo -u vagrant wget -q https://github.com/flodruid/vagrant/blob/main/001_files/info.php
+sudo -u vagrant wget -q https://raw.githubusercontent.com/flodruid/vagrant/main/001_files/info.php
 
 
 service httpd restart
